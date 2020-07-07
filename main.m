e023878c989b94fa0693aa021d38d2e8bd183eef
awaispk <m.awais@kaist.ac.kr>
2020-07-07 15:12:01 +0900

    New functions added to display Estimation Error, 3-Sigma bounds and Biases together

--- main.m
+++ main.m
@@ -7,6 +7,7 @@ function [] = main()
 % Advisor:          Prof. Hyuchoong Bang
 % email:            m.awais@kaist.ac.kr
 % Code Ref:         https://openimu.readthedocs.io/en/latest/algorithms.html
+% Useful Ref(s):    https://www.telesens.co/category/sensor-fusion/
 
 % State Vector
 % x = [r, v, q, wb, ab] State Vector, (1 x 16)
@@ -43,8 +44,8 @@ stdEuler = [0.01, 0.01, 0.01]; %Std. dev. of euler ang. meas. error, rad/sec
 % --> Detailed specs are available in associated sensor measurement functions
 
 % Simulation Parameters
-N = 500;     % (dataSamples-1), 1st smaple used for initilization
-M = 10;	% Number of Monte-Carlo runs
+N = 100;     % (dataSamples-1), 1st smaple used for initilization
+M = 5;	% Number of Monte-Carlo runs
 dt = 0.1;   % Sample Time
 
 %% Extended Kalman Filter simulation
@@ -76,7 +77,7 @@ for m = 1:1:M
     PDiagIni = diag(PPrev);
     wMeasPrev = wMeas;
     aMeasPrev = aMeas;
-    for k = 1:1:N
+    for n = 1:1:N
         %%Prediction
         %Step 1a: State Prediction
         xPred = stateTransMdl(xPrev, dt, aMeasPrev, wMeasPrev);
@@ -88,7 +89,7 @@ for m = 1:1:M
         
         %%Update
         %Step 3: Measurement Readout
-        [zMeas, wMeas, aMeas, zTrue, R] = measSensorReading(k, gpsLLARef);
+        [zMeas, wMeas, aMeas, zTrue, R] = measSensorReading(n, gpsLLARef);
         H = observationMdl(xPred)
        
         %Step 4: Measurement Prediction
@@ -99,21 +100,21 @@ for m = 1:1:M
         K = PPred*H'/S;     %Kalman Gain
 
         %Step 7: State Estimation/Update
-        xEst(:,k) = xPred + K * (zMeas - h);
-        xTrue(:,k) = xPred + K * (zTrue - h); % Just for analysis
+        xEst(:,n) = xPred + K * (zMeas - h);
+        xTrue(:,n) = xPred + K * (zTrue - h); % Just for analysis
 
         %Step 8: Error Covariance Estimation/Update
         PEst = (eye(16) - K*H)*PPred;
-        PDiag(:,k) = diag(PEst);
+        PDiag(:,n) = diag(PEst);
  
-        xPrev = xEst(:,k);
+        xPrev = xEst(:,n);
         PPrev = PEst;
         wMeasPrev = wMeas;
         aMeasPrev = aMeas;
     end
     PDiagComp = [PDiagIni, PDiag];
     xEstComp = [xErrIni, xEst];
-    xTrueComp = [xTrueIni, xTrue]; %For analysis only
+    xTrueComp = [xTrueIni, xTrue]; %For analysis only, No practical use
     resXEst(:,:,m) = xEstComp;
     resXTrue(:,:,m) = xTrueComp;    %For analysis only
     resXEstErr(:,:,m) = xEstComp - xTrueComp;
@@ -122,16 +123,21 @@ end
 
 %% get result statistics
 xEstAVG = mean(resXEst,3); %Average of all monte carlo runs
-xEstErr_AVG = mean(resXEstErr,3); %Average of all monte carlo runs
+xEstErrAVG = mean(resXEstErr,3); %Average of all monte carlo runs
 x_RMSE = zeros(size(resXEstErr, 1),N+1); % root mean square error
 PDiagAVG = mean(resPDiag,3);
-% xTrueAVG = mean(resXTrue,3); %Meaningless quantity. 4
+xTrue = mean(resXTrue,3); %Meaningless quantity. 4
 
-for k = 1:1:N+1
+for n = 1:1:N+1
+    xEstAVG(17:19,n) = quat2euler(xEstAVG(7:10));
+    xEstErrAVG(17:19,n) = quat2euler(xEstErrAVG(7:10));
+    xTrue(17:19,n) = quat2euler(xTrue(7:10));
     for m = 1:1:size(resXEstErr, 1)
-        x_RMSE(m,k) = sqrt(mean(resXEstErr(m,k,:).^2,3));
+        x_RMSE(m,n) = sqrt(mean(resXEstErr(m,n,:).^2,3));
     end
- end
+    x_RMSE(17:19,n) = quat2euler(x_RMSE(7:10));
+end
+
 
 %% plot results
 time = (0:1:N)*dt;
@@ -139,41 +145,61 @@ time = (0:1:N)*dt;
 % dispEstStates(NoOfSamples, sampleTime, stateNum)
 NoOfSamples = N;
 sampleTime = dt;
-stateNum = [1:3];
-xPosIdx = [1:3];
-xVelIdx = [4:6];
-xQuatIdx = [7:10];
-xWBIdx = [11:13];
-xABIdx = [14:16];
 
-dispEstStates(resXTrue, resXEst, NoOfSamples, sampleTime, xPosIdx); suptitle('True/Estimated Position, ');
-dispEstStates(resXTrue, resXEst, NoOfSamples, sampleTime, xVelIdx); suptitle('True/Estimated Velocity, ');
-dispEstStates(resXTrue, resXEst, NoOfSamples, sampleTime, xQuatIdx); suptitle('True/Estimated Quaternion, ');
-dispEstStates(resXTrue, resXEst, NoOfSamples, sampleTime, xWBIdx); suptitle('True/Estimated Gyro Bias, ');
-dispEstStates(resXTrue, resXEst, NoOfSamples, sampleTime, xABIdx); suptitle('True/Estimated Accelerometer Bias, ');
+xPosIdx = 1:3;
+xVelIdx = 4:6;
+xQuatIdx = 7:10;
+xWBIdx = 11:13;
+xABIdx = 14:16;
+xEulerIdx = 17:19;
+
+dispEstStates(xTrue, xEstAVG, NoOfSamples, sampleTime, xPosIdx); suptitle('True/Estimated Position, ');
+dispEstStates(xTrue, xEstAVG, NoOfSamples, sampleTime, xVelIdx); suptitle('True/Estimated Velocity, ');
+dispEstStates(xTrue, xEstAVG, NoOfSamples, sampleTime, xQuatIdx); suptitle('True/Estimated Quaternion, ');
+dispEstStates(xTrue, xEstAVG, NoOfSamples, sampleTime, xEulerIdx); suptitle('True/Estimated Euler, ');
+dispEstStates(xTrue, xEstAVG, NoOfSamples, sampleTime, xWBIdx); suptitle('True/Estimated Gyro Bias, ');
+dispEstStates(xTrue, xEstAVG, NoOfSamples, sampleTime, xABIdx); suptitle('True/Estimated Accelerometer Bias, ');
 
 dispEstErrors(x_RMSE, PDiagComp, NoOfSamples, sampleTime, xPosIdx);  suptitle('Standard Deviation of Position Errors, ');
 dispEstErrors(x_RMSE, PDiagComp, NoOfSamples, sampleTime, xVelIdx);  suptitle('Standard Deviation of Velocity Errors, ');
 dispEstErrors(x_RMSE, PDiagComp, NoOfSamples, sampleTime, xQuatIdx);  suptitle('Standard Deviation of Quaternion Errors, ');
+% dispEstErrors(x_RMSE, PDiagComp, NoOfSamples, sampleTime, xEulerIdx);suptitle('Standard Deviation of Euler Errors, ');
+% Covariance propagation from quaternion to euler is pending yet
 dispEstErrors(x_RMSE, PDiagComp, NoOfSamples, sampleTime, xWBIdx);  suptitle('Standard Deviation of Gyro Bias Errors, ');
 dispEstErrors(x_RMSE, PDiagComp, NoOfSamples, sampleTime, xABIdx);  suptitle('Standard Deviation of Acclerometer Bias Errors, ');
 
+dispEstErr_3SigmaBound_AccBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xPosIdx); suptitle('Position Error with 3-Sigma Bounds View and Absolute Accelerometer Bias');
+dispEstErr_3SigmaBound_AccBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xVelIdx); suptitle('Velocity Error with 3-Sigma Bounds View and Absolute Accelerometer Bias');
+dispEstErr_3SigmaBound_AccBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xQuatIdx); suptitle('Quaternion Error with 3-Sigma Bounds View and Absolute Accelerometer Bias');
+% dispEstErr_3SigmaBound_AccBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xEulerIdx); suptitle('Euler Error with 3-Sigma Bounds View and Absolute Accelerometer Bias');
+% Covariance propagation from quaternion to euler is pending yet
+dispEstErr_3SigmaBound_AccBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xWBIdx); suptitle('Acceleration Bias Error with 3-Sigma Bounds View and Absolute Accelerometer Bias');
+dispEstErr_3SigmaBound_AccBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xABIdx); suptitle('Angular Velocity Error with 3-Sigma Bounds View and Absolute Accelerometer Bias');
+
+
+dispEstErr_3SigmaBound_GyroBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xPosIdx); suptitle('Position Error with 3-Sigma Bounds View and Absolute Gyro Bias');
+dispEstErr_3SigmaBound_GyroBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xVelIdx); suptitle('Velocity Error with 3-Sigma Bounds View and Absolute Gyro Bias');
+dispEstErr_3SigmaBound_GyroBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xQuatIdx); suptitle('Quaternion Error with 3-Sigma Bounds View and Absolute Gyro Bias');
+% dispEstErr_3SigmaBound_GyroBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xEulerIdx); suptitle('Euler Error with 3-Sigma Bounds View and Absolute Gyro Bias');
+% Covariance propagation from quaternion to euler is pending yet
+dispEstErr_3SigmaBound_GyroBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xWBIdx); suptitle('Acceleration Bias Error with 3-Sigma Bounds View and Absolute Gyro Bias');
+dispEstErr_3SigmaBound_GyroBias(xEstAVG, xEstErrAVG, PDiagComp, NoOfSamples, sampleTime, xABIdx); suptitle('Angular Velocity Error with 3-Sigma Bounds View and Absolute Gyro Bias');
 
 dispEstStates3D(resXTrue, resXEst, xPosIdx); suptitle('True/Estimated Position, ');
 dispEstStates3D(resXTrue, resXEst, xVelIdx); suptitle('True/Estimated Velocity, ');
-
+% 
 
 
 end
 
 
-function dispEstStates(resXTrue, resXEst, NoOfSamples, sampleTime, stateNum)
+function dispEstStates(xTrueAVG, xEstAVG, NoOfSamples, sampleTime, stateNum)
 figure('Name', 'Time history of an estimation results');
 time = (0:1:NoOfSamples)*sampleTime;
 for n = 1:1:length(stateNum)
     subplot(length(stateNum),1,n); hold on;
-    plot(time, resXTrue(stateNum(n),:, 1), 'linewidth', 2);
-    plot(time, resXEst(stateNum(n),:,1), '--', 'linewidth', 2);
+    plot(time, xTrueAVG(stateNum(n),:), 'linewidth', 2);
+    plot(time, xEstAVG(stateNum(n),:), '--', 'linewidth', 2);
     legend({'True', 'Estimated'}, 'fontsize', 12);
     xlabel('Time (sec)', 'fontsize', 12);
     ylabel(strcat('X', num2str(stateNum(n), '%0.2d')), 'fontsize', 12); grid on;
@@ -189,6 +215,7 @@ for n = 1:1:length(stateNum)
     subplot(length(stateNum),1,n); hold on;
     plot(time, x_RMSE(stateNum(n),:), 'linewidth', 2);
     plot(time, sqrt(PDiagComp(stateNum(n),:)), '--', 'linewidth', 2);
+    
     legend({'RMSE Estimation', 'RMSE Process'}, 'fontsize', 12);
     xlabel('Time (sec)', 'fontsize', 12);
     ylabel(strcat('X', num2str(stateNum(n), '%0.2d'), ' Error Std'), 'fontsize', 12); grid on;
@@ -207,3 +234,114 @@ legend({'True', 'Estimated'}, 'fontsize', 12);
 xlabel('X', 'fontsize', 12); ylabel('Y', 'fontsize', 12); zlabel('Z', 'fontsize', 12);
 grid on;
 end
+
+
+function dispEstErr_3SigmaBound_AccBias(xEstAVG, xEstErr_AVG, PDiagComp, NoOfSamples, sampleTime, stateNum)
+figure('Name','Estimate errors along with 3-sigma bounds and Accelerometer Bias');
+time = (0:1:NoOfSamples)*sampleTime;
+
+
+for n = 1:1:length(stateNum)
+    subplot(length(stateNum),1,n); hold on;
+    e = plot(time, xEstErr_AVG(stateNum(n),:), 'k-',  'linewidth', 2);
+    b1 = plot(time, 3*sqrt(PDiagComp(stateNum(n),:)), 'r:', 'linewidth', 2);
+    plot(time, -3*sqrt(PDiagComp(stateNum(n),:)), 'r:', 'linewidth', 2);
+
+    if (n >= 1) && (n <= 3)         %Position
+        biasAcc = plot(time, xEstAVG(13+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 4) && (n <= 6)     %Velocity
+        biasAcc = plot(time, xEstAVG(10+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 7) && (n <= 10)    %Quaternion
+        biasAcc = plot(time, xEstAVG(7+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 11) && (n <= 13)   %Accelerometer Bias
+        biasAcc = plot(time, xEstAVG(3+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 14) && (n <= 16)   %Gyro Bias
+        bias = plot(time, xEstAVG(0+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 17) && (n <= 19)   %Euler Angles
+        biasAcc = plot(time, xEstAVG(-3+n,:), 'b--', 'linewidth', 2);
+    end
+    legend([e b1 biasAcc], {'Estimation Error', '3-Sigma Bounds', 'Accelerometer Bias'}, 'fontsize', 12);
+    xlabel('Time (sec)', 'fontsize', 12);
+    ylabel(strcat('X', num2str(stateNum(n), '%0.2d'), ' '), 'fontsize', 12); grid on;
+end
+
+end
+
+function dispEstErr_3SigmaBound_GyroBias(xEstAVG, xEstErr_AVG, PDiagComp, NoOfSamples, sampleTime, stateNum)
+figure('Name','Estimate errors along with 3-sigma bounds and Gyro Bias');
+time = (0:1:NoOfSamples)*sampleTime;
+
+
+for n = 1:1:length(stateNum)
+    subplot(length(stateNum),1,n); hold on;
+    e = plot(time, xEstErr_AVG(stateNum(n),:), 'k-',  'linewidth', 2);
+    b1 = plot(time, 3*sqrt(PDiagComp(stateNum(n),:)), 'r:', 'linewidth', 2);
+    plot(time, -3*sqrt(PDiagComp(stateNum(n),:)), 'r:', 'linewidth', 2);
+
+    if (n >= 1) && (n <= 3)         %Position
+        biasGyro = plot(time, xEstAVG(10+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 4) && (n <= 6)     %Velocity
+        biasGyro = plot(time, xEstAVG(7+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 7) && (n <= 10)    %Quaternion
+        biasGyro = plot(time, xEstAVG(4+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 11) && (n <= 13)   %Accelerometer Bias
+        biasGyro = plot(time, xEstAVG(0+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 14) && (n <= 16)   %Gyro Bias
+        biasGyro = plot(time, xEstAVG(-3+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 17) && (n <= 19)   %Euler Angles
+        biasGyro = plot(time, xEstAVG(-6+n,:), 'b--', 'linewidth', 2);
+    end
+    legend([e b1 biasGyro], {'Estimation Error', '3-Sigma Bounds', 'Gyro Bias'}, 'fontsize', 12);
+    xlabel('Time (sec)', 'fontsize', 12);
+    ylabel(strcat('X', num2str(stateNum(n), '%0.2d'), ' '), 'fontsize', 12); grid on;
+end
+
+end
+
+function dispEstErr_3SigmaBound_AccGyroBias(xEstAVG, xEstErr_AVG, PDiagComp, NoOfSamples, sampleTime, stateNum)
+figure('Name','Estimate errors along with 3-sigma bounds and Accelerometer Bias');
+time = (0:1:NoOfSamples)*sampleTime;
+
+
+for n = 1:1:length(stateNum)
+    subplot(length(stateNum),1,n); hold on;
+    e = plot(time, xEstErr_AVG(stateNum(n),:), 'k-',  'linewidth', 2);
+    b1 = plot(time, 3*sqrt(PDiagComp(stateNum(n),:)), 'r:', 'linewidth', 2);
+    plot(time, -3*sqrt(PDiagComp(stateNum(n),:)), 'r:', 'linewidth', 2);
+
+    if (n >= 1) && (n <= 3)         %Position
+        biasAcc = plot(time, xEstAVG(13+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 4) && (n <= 6)     %Velocity
+        biasAcc = plot(time, xEstAVG(10+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 7) && (n <= 10)    %Quaternion
+        biasAcc = plot(time, xEstAVG(7+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 11) && (n <= 13)   %Accelerometer Bias
+        biasAcc = plot(time, xEstAVG(3+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 14) && (n <= 16)   %Gyro Bias
+        bias = plot(time, xEstAVG(0+n,:), 'b--', 'linewidth', 2);
+    elseif (n >= 17) && (n <= 19)   %Euler Angles
+        biasAcc = plot(time, xEstAVG(-3+n,:), 'b--', 'linewidth', 2);
+    end
+    
+        if (n >= 1) && (n <= 3)         %Position
+        biasGyro = plot(time, xEstAVG(10+n,:), 'm--', 'linewidth', 2);
+    elseif (n >= 4) && (n <= 6)     %Velocity
+        biasGyro = plot(time, xEstAVG(7+n,:), 'm--', 'linewidth', 2);
+    elseif (n >= 7) && (n <= 10)    %Quaternion
+        biasGyro = plot(time, xEstAVG(4+n,:), 'm--', 'linewidth', 2);
+    elseif (n >= 11) && (n <= 13)   %Accelerometer Bias
+        biasGyro = plot(time, xEstAVG(0+n,:), 'm--', 'linewidth', 2);
+    elseif (n >= 14) && (n <= 16)   %Gyro Bias
+        biasGyro = plot(time, xEstAVG(-3+n,:), 'm--', 'linewidth', 2);
+    elseif (n >= 17) && (n <= 19)   %Euler Angles
+        biasGyro = plot(time, xEstAVG(-6+n,:), 'm--', 'linewidth', 2);
+        end
+    
+        
+    legend([e b1 biasAcc biasGyro], {'Estimation Error', '3-Sigma Bounds', 'Accelerometer Bias', 'Gyro Bias'}, 'fontsize', 12);
+    xlabel('Time (sec)', 'fontsize', 12);
+    ylabel(strcat('X', num2str(stateNum(n), '%0.2d'), ' '), 'fontsize', 12); grid on;
+end
+
+end
+
